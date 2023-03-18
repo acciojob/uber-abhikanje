f209633b2b0d4e8f2a4d21944fe09359154d17d4
ABHISHEK KANAJE <102962455+abhikanje@users.noreply.github.com>
2023-03-18 21:41:39 +0530

    Update DriverServiceImpl.java

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -1,13 +1,10 @@
 package com.driver.services.impl;
 
-import com.driver.model.Cab;
-import com.driver.repository.CabRepository;
-import com.driver.services.DriverService;
+import com.driver.model.*;
+import com.driver.repository.*;
+import com.driver.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.driver.model.Driver;
-import com.driver.repository.DriverRepository;
 
 @Service
 public class DriverServiceImpl implements DriverService {
@@ -19,20 +16,42 @@ public class DriverServiceImpl implements DriverService {
 	CabRepository cabRepository3;
 
 	@Override
-	public void register(String mobile, String password){
-		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+	public void register(String mobile, String password) {
+		// Save a driver in the database having given details and a cab with ratePerKm
+		// as 10 and availability as True by default.
+		Driver driver = new Driver();
+		driver.setMobile(mobile);
+		driver.setPassword(password);
 
+		Cab cab = new Cab();
+		cab.setAvailable(true);
+		cab.setPerKmRate(10);
+		cab.setDriver(driver);
+
+		driver.setCab(cab);
+		driverRepository3.save(driver);
 	}
 
 	@Override
-	public void removeDriver(int driverId){
+	public void removeDriver(int driverId) {
 		// Delete driver without using deleteById function
+		Driver driver = driverRepository3.findById(driverId).get();
+		if (driver == null) {
+			return;
+		}
+		driverRepository3.delete(driver);
 
+		Cab cab = driver.getCab();
+		cabRepository3.delete(cab);
 	}
 
 	@Override
-	public void updateStatus(int driverId){
-		//Set the status of respective car to unavailable
+	public void updateStatus(int driverId) {
+		// Set the status of respective car to unavailable
+		Driver driver = driverRepository3.findById(driverId).get();
+		Cab cab = driver.getCab();
+		cab.setAvailable(false);
+		cabRepository3.save(cab);
 
 	}
 }

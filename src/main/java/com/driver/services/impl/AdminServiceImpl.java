9a86f9c8a35753f9f2c3fd9ca7a45fac77687c45
ABHISHEK KANAJE <102962455+abhikanje@users.noreply.github.com>
2023-03-18 21:40:16 +0530

    Update AdminServiceImpl.java

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,11 +1,9 @@
 package com.driver.services.impl;
 
 import java.util.List;
-
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.driver.model.Admin;
 import com.driver.model.Customer;
 import com.driver.model.Driver;
@@ -27,31 +25,40 @@ public class AdminServiceImpl implements AdminService {
 
 	@Override
 	public void adminRegister(Admin admin) {
-		//Save the admin in the database
+		// Save the admin in the database
+		adminRepository1.save(admin);
 	}
 
 	@Override
 	public Admin updatePassword(Integer adminId, String password) {
-		//Update the password of admin with given id
-
+		// Update the password of admin with given id
+		Admin admin = adminRepository1.findById(adminId).get();
+		admin.setPassword(password);
+		adminRepository1.save(admin);
+		return admin;
 	}
 
 	@Override
-	public void deleteAdmin(int adminId){
+	public void deleteAdmin(int adminId) {
 		// Delete admin without using deleteById function
-
+		Admin admin = adminRepository1.findById(adminId).get();
+		if (admin != null) {
+			adminRepository1.delete(admin);
+		}
 	}
 
 	@Override
 	public List<Driver> getListOfDrivers() {
-		//Find the list of all drivers
-
+		// Find the list of all drivers
+		List<Driver> drivers = driverRepository1.findAll();
+		return drivers;
 	}
 
 	@Override
 	public List<Customer> getListOfCustomers() {
-		//Find the list of all customers
-
+		// Find the list of all customers
+		List<Customer> customers = customerRepository1.findAll();
+		return customers;
 	}
 
 }
